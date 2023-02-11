e2e6e8838e7e6ef29d3e04ea5958b8b17de1ab65
Francesco Saverio Garzi Cosentino <garzicosentino@gmail.com>
2023-02-11 03:26:43 +0100

    update

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 22:50:39 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/02/09 21:54:18 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/02/11 03:26:05 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,18 @@
 int	ft_printf(const char *str, ...)
 {
 	va_list		ap;
-	int			i;
 	int			len;
 	
 	if (!str)
 		return (0);
 	va_start(ap, str);
 	len = 0;
-	i = -1;
-	while (str[++i])
+	while (*str)
 	{
-		if(str[i] == 37)
-		{
-			if (!find_specifier(&(str[++i]), &len, &ap))
-				return (len);
-		}
-		else
-		{
-			write(1, &str[i], 1);
-			len++;
-		}
+		if (*str != 37 && ++len)
+			write(1, str++, 1);
+		else if (!find_specifier(++str, &len, &ap))
+			return (len);
 	}
 	va_end(ap);
 	return (len);
